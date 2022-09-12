c114ea03f4b953dae456938c10f0e08b0325a110
IceBear9028 <tlatms9028@gmail.com>
2022-09-12 16:37:35 +0900

    4 commit
    open cv2 이용해서 브라우저 환경에 웹캠을 띄울려고 하였지만, 제대로 구현이 되지 않는 상태
    => 예상 이유 1 : 바닐라 js 에서는 Local 에서 바로 열리지만, dash 에서는 서버를 통해서 열린다. 따라서, 웹캠 이미지가 서버로 이동하지 않고 막힌것 같음
    ++ 아 ㅅㅂ 모르겠음.
    ++ 일단, html.Video 기능은 css에 대해 별다른 에러는 없었음. 그냥 가로, 세로는 원하는 크기로 설정하면 됨

--- app_activate.py
+++ app_activate.py
@@ -1,58 +1,79 @@
 from dash import Dash, dcc, html, Input, Output, dash_table
 import plotly.express as px
-import plotly.graph_objects as go
 import cv2
+from flask import Flask, Response
 
 class VideoCamera(object):
-    
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
 
-app = Dash(__name__)
+    def __del__(self):
+        self.video.release()
+        cv2.destroyAllWindows()
+
+    def get_frame(self):
+        sucess, image = self.video.read()
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+def video_feed():
+    return Response(gen(VideoCamera()), 
+                    mimetype='multipart/x-mixed-replace; boundary=frame'
+    )
+
+server = Flask(__name__)
+app = Dash(__name__, server=server)
 
 app.layout = html.Div(
-    className = "main",
-    children = [
-        html.Video(
-            className = 'webCam',
-            autoPlay = True,
-            muted = 'muted',
+    className="main",
+    children=[
+        html.Div(
+            className='title',
+            children=[html.H1("시발람들아")],
+            style={
+                'width': '1500px',
+                'justify-content': 'flex-start',
+                'padding-left': '20px'
+            }
         ),
         html.Div(
-          className = 'title',
-          children = [html.H1("시발람들아")],
-          style = {
-            'width' : '1500px',
-            'justify-content' : 'flex-start',
-            'padding-left' : '20px'
-          }
-        ),
-        html.Div(
-            className = 'container',
-            children = [
+            className='container',
+            children=[
                 html.Div(
-                    className = "graphDiv",
-                    children  = [
-                        html.Div(className = "graph1"),
-                        html.Div(className = "graph2")
+                    className="graphDiv",
+                    children=[
+                        html.Div(
+                            className="graph1",
+                            children = [
+                                html.H1("그래프1")
+                            ]
+                            ),
+                        html.Div(
+                            className="graph2",
+                            children = [
+                                html.H1("그래프2")
+                            ]
+                            )
                     ]
                 ),
-                html.Div(
-                    className = 'webCamContainer',
-                    children = [
-                    #     html.Video(
-                    #         className = 'webCam',
-                    #         autoPlay = True,
-                    #         muted = True
-                    #     )
-                    ],
+                html.Img(
+                    src = './video_feed',
                     style = {
-                        'height' : '500px',
-                        'width' : '300px',
+                        'width' : '400px',
+                        'height' : '280px'
                     }
                 )
             ]
         )
     ]
 )
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
