b34c320e441bc1976356c32542aa46c76ba6cd96
IceBear9028 <tlatms9028@gmail.com>
2022-09-15 22:53:00 +0900

    5 commit
    현재 웹캠으로 하는거 나중에 구현하는 것으로 결정
    현재 css 로 약간의 div 구간을 나누었음.
    
    ++ 일단, 실시간으로 그래프가 띄어지는 방법 찾아볼 것

--- app_activate.py
+++ app_activate.py
@@ -3,30 +3,38 @@ import plotly.express as px
 import cv2
 from flask import Flask, Response
 
-class VideoCamera(object):
-    def __init__(self):
-        self.video = cv2.VideoCapture(0)
+# class VideoCamera(object):
+#     def __init__(self):
+#         self.video = cv2.VideoCapture(0)
 
-    def __del__(self):
-        self.video.release()
-        cv2.destroyAllWindows()
+#     def __del__(self):
+#         self.video.release()
+#         cv2.destroyAllWindows()
 
-    def get_frame(self):
-        sucess, image = self.video.read()
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+#     def get_frame(self):
+#         sucess, image = self.video.read()
+#         ret, jpeg = cv2.imencode('.jpg', image)
+#         return jpeg.tobytes()
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+# def gen(camera):
+#     while True:
+#         frame = camera.get_frame()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-def video_feed():
-    return Response(gen(VideoCamera()), 
-                    mimetype='multipart/x-mixed-replace; boundary=frame'
-    )
+# def video_feed():
+#     return Response(gen(VideoCamera()), 
+#                     mimetype='multipart/x-mixed-replace; boundary=frame'
+#     )
 
+# 1. 데이터 프레임 불러오기
+
+# 2. 그래프 저장
+graph_id_1 = 'focus1'
+graph_id_2 = 'focus2'
+
+
+# 3. 웹 레이아웃 설정
 server = Flask(__name__)
 app = Dash(__name__, server=server)
 
@@ -35,7 +43,7 @@ app.layout = html.Div(
     children=[
         html.Div(
             className='title',
-            children=[html.H1("시발람들아")],
+            children=[html.H1("focus-capstone")],
             style={
                 'width': '1500px',
                 'justify-content': 'flex-start',
@@ -46,35 +54,63 @@ app.layout = html.Div(
             className='container',
             children=[
                 html.Div(
+                    className = 'webCamContainer',
+                    children = [
+                        html.Div(
+                            style = {
+                                'width' : '100%',
+                                'height' : '100%'
+                            }
+                        )
+                    ]
+                ),
+                html.Div(
                     className="graphDiv",
                     children=[
                         html.Div(
                             className="graph1",
                             children = [
-                                html.H1("그래프1")
+                                dcc.Graph(
+                                    id = graph_id_1,
+                                    style = {
+                                        'width':'90%',
+                                        'height' : '80%',
+                                    }
+                                )
                             ]
                             ),
                         html.Div(
                             className="graph2",
                             children = [
-                                html.H1("그래프2")
+                                dcc.Graph(
+                                    id = graph_id_2,
+                                    style = {
+                                        'width' : '90%',
+                                        'height' : '80%'
+                                    }
+                                )
                             ]
                             )
                     ]
                 ),
-                html.Img(
-                    src = './video_feed',
-                    style = {
-                        'width' : '400px',
-                        'height' : '280px'
-                    }
-                )
             ]
         )
     ]
 )
 # 참고할 웹사이트
 # http://wandlab.com/blog/?p=94
+
+# 4. 그래프 속성 설정
+@app.callback(
+    Output(graph_id_1, 'figure')
+)
+def focus_1():
+    fig = px.line()
+    fig.update_layout(
+        margin = dict(l=10,r=10, t=10, b=10),
+        
+    )
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- assets/custom-script.js
+++ assets/custom-script.js
@@ -1,13 +1,14 @@
-const video = document.getElementsByClassName('webCam');
+// const video = document.getElementsByClassName('webCam');
 
-if (navigator.mediaDevices.getUserMedia) {
-	navigator.mediaDevices.getUserMedia({ video: true })
-        .then( (stream) => { 
-          video.srcObject = stream;
-    	})
-  		.catch(function (error) {
-          console.log("Something went wrong!");
-          console.log(error);
-          return;
-        });
-};
+// if (navigator.mediaDevices.getUserMedia) {
+// 	navigator.mediaDevices.getUserMedia({ video: true })
+//         .then( (stream) => { 
+//           video.srcObject = stream;
+//     	})
+//   		.catch(function (error) {
+//           console.log("Something went wrong!");
+//           console.log(error);
+//           return;
+//         });
+// };
+
