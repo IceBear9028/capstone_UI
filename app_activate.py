3176dc58a3060fe109e01f6a589e8d6e9185dced
IceBear9028 <tlatms9028@gmail.com>
2022-11-11 05:27:44 +0900

    section_focus_graph commit 1
    1. 현재, section 을 선택했을 때, section 에 대한 집중도 그래프를 띄우는 작업 진행중
    2. 그래서, section 의 집중도 그래프를 띄울 수 있게 div 위치 조정 작업 진행
    3. 자잘자잘한 css 수정

--- app_activate.py
+++ app_activate.py
@@ -404,7 +404,7 @@ def stream_gen( src ):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
 
 
 
--- focus_page.py
+++ focus_page.py
@@ -14,19 +14,12 @@ layout = html.Div(
             ]
         ),
         html.Div(
-            className = 'graphContainer',
-            children = [
-                dcc.Graph(
-                    id = 'focus1',
-                    style = {
-                        'border-radius' : '15px', 
-                    }
-                ),
-            ]
-        ),
-        html.Div(
             className = "focusProbViewContainer",
             children = [
+                html.Div(
+                    className = 'sectionFocusProbBoard',
+                    children = []
+                ),
                 html.Div(
                     className = 'titleContainer',
                     children = [
@@ -39,48 +32,65 @@ layout = html.Div(
                 html.Div(
                     className = 'focusProbBoard',
                     children = [
-                        html.Img(
-                            src = "/video",
-                            id = "facePhoto",
-                            style = {
-                                'border' : '1px solid #ddd',
-                                "border-radius" : '10px',
-                                'padding' : '5px',
-                                'width' : '220px',
-                                # 'box-shadow' :  '0 5px 18px -7px rgb(185, 185, 185)',
-                            }
+                        html.Div(
+                            className = 'currentElementContainer',
+                            children = [
+                                html.Img(
+                                    src = "/video",
+                                    id = "facePhoto",
+                                    style = {
+                                        'border' : '1px solid #ddd',
+                                        "border-radius" : '10px',
+                                        'padding' : '5px',
+                                        'width' : '220px',
+                                        'box-shadow' :  '0 5px 18px -7px rgb(185, 185, 185)',
+                                    },
+                                ),
+                                html.Div(
+                                    className = 'focusFigureContainer',
+                                    children = [
+                                        html.Div(
+                                            id = 'currentFocusFigure',
+                                            children = [
+                                                html.H3(
+                                                    id = 'currentFocus Index',
+                                                    children = ['현재집중도 :']
+                                                ),
+                                                html.H3(
+                                                    id = 'currentFocus Result',
+                                                    children = []
+                                                )
+                                            ]
+                                        ),
+                                        html.Div(
+                                            id = "durationTimeFigure",
+                                            children = [
+                                                html.H3(
+                                                    id = 'durationTime Index',
+                                                    children = ['학습시간 :']
+                                                ),
+                                                html.H3(
+                                                    id = 'durationTime Result',
+                                                    children = []
+                                                )
+                                            ]
+                                        )
+                                    ]
+                                ),
+                            ]
                         ),
                         html.Div(id ='divSpace_0'),
                         html.Div(id ='divSpace_1'),
                         html.Div(
-                            className = 'focusFigureContainer',
+                            className = 'graphContainer',
                             children = [
-                                html.Div(
-                                    id = 'currentFocusFigure',
-                                    children = [
-                                        html.H3(
-                                            id = 'currentFocus Index',
-                                            children = ['현재집중도 :']
-                                        ),
-                                        html.H3(
-                                            id = 'currentFocus Result',
-                                            children = []
-                                        )
-                                    ]
+                                dcc.Graph(
+                                    id = 'focus1',
+                                    style = {
+                                        'border-radius' : '15px',
+                                        'height' : '300px'
+                                    }
                                 ),
-                                html.Div(
-                                    id = "durationTimeFigure",
-                                    children = [
-                                        html.H3(
-                                            id = 'durationTime Index',
-                                            children = ['학습시간 :']
-                                        ),
-                                        html.H3(
-                                            id = 'durationTime Result',
-                                            children = []
-                                        )
-                                    ]
-                                )
                             ]
                         ),
                     ]
